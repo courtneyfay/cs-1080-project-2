1f445580f1f4a159ae43af011a0094ec6572f355
Courtney Fay <courtneyfay@users.noreply.github.com>
2024-06-24 12:20:19 -0400

    looping through character cards array

--- app.js
+++ app.js
@@ -17,21 +17,21 @@ const characters = [
 ]
 
 const showPlayerGameBoard = () => {
-    console.log('hitting showPlayerGameBoard')
-    const gameBoard = document.querySelector(".player-game-board")
-    // todo: make this a loop and do it for all character cards
-    // 1. create character card div and add class
-    const characterCard = document.createElement('div')
-    characterCard.classList.add('character-card')
-    // 2. add name to character card div
-    const name = document.createElement('p')
-    name.innerHTML = characters[0].name
-    characterCard.appendChild(name)
-    // 3. add color to character card div
-    characterCard.style.background = characters[0].color
-    // style.background = color;
-    // 4. append character card div to player game board div
-    gameBoard.appendChild(characterCard)
+    const gameBoard = document.querySelector('.player-game-board')
+    // loop through all character cards
+    for (let i = 0; i < characters.length; i++) {
+        // 1. create character card div and add class
+        const characterCard = document.createElement('div')
+        characterCard.classList.add('character-card')
+        // 2. add name to character card div
+        const name = document.createElement('p')
+        name.innerHTML = characters[i].name
+        characterCard.appendChild(name)
+        // 3. add color to character card div
+        characterCard.style.background = characters[i].color
+        // 4. append character card div to player game board div
+        gameBoard.appendChild(characterCard)
+    }
 }
 
 const startGame = () => {
